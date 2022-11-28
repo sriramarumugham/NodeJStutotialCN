1c160c9ae0079b103d8788188b7b86dc8a5cbcdf
Sreeram A <sriramlibra0@gmail.com>
2022-11-28 14:17:56 +0530

    authentication post                                                                                                     before postig the form sumbit action

--- Major Project/passport.js/codeial/routes/post.js
+++ Major Project/passport.js/codeial/routes/post.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/post_controller");
+const passport=require('../config/passport-local-strategy');
 
-router.post("/create", postController.create);
+router.post("/create",  passport.checkAuthentication , postController.create);
 
 module.exports = router;
