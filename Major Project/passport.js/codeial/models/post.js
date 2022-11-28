c3f3d75ce0e7bc23fffe654591bf1e985bac397b
Sreeram A <sriramlibra0@gmail.com>
2022-11-28 12:25:38 +0530

    creating post

---
+++ Major Project/passport.js/codeial/models/post.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+
+const PostSchema=new mongoose.Schema({
+
+    content:{
+            type:String,
+            required:true,
+    },
+    user:{
+           type:mongoose.Schema.Types.ObjectId,
+           ref:'User'
+    }
+} , {timestamps:true});
+
+const Post=mongoose.model('Post', PostSchema);
+
+module.exports=Post;
